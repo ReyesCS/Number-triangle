76e8127ec33baef684c7f81e75ae99908c812ee2
ReyesCS <reyes1561@yahoo.com>
2017-07-18 09:17:05 -0700

    First Commit

---
+++ Printing crazy numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printing_crazy_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pinting a numbers triangle
+            Console.Write("input number of rows: ");
+
+            int rows = Convert.ToInt32
+                (Console.ReadLine());
+
+            for (int i = 1; i < rows; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                    Console.Write(i);
+                Console.Write("\n");
+
+            }
+        }
+    }
+}
